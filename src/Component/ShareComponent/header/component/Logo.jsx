31fb7ff7cf7ba23b99d38ad45bd331e3cf49e8eb
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-08-17 12:55:43 +0600

    logo and favicon fixed

--- src/Component/ShareComponent/header/component/Logo.jsx
+++ src/Component/ShareComponent/header/component/Logo.jsx
@@ -1,18 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className='logo-wapper'>
-      <img
-        className='w-10 h-10'
-        src='https://i.ibb.co/ZYgSW6L/IMG-20220218-WA0000-removebg-preview.png'
-        alt=''
-      />
-      <Link to='/'>
+    <Link to="/">
+      <div className="logo-wapper">
+        <img className="size-14" src="/logo.png" alt="" />
         <span>CYCLE MART</span>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
